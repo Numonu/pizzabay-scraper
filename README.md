a0b98d5c2184315ddfd2faf91f5287a7494dba5b
Numonu <juan.villegas.dev@gmail.com>
2023-06-07 17:42:48 -0500

    (build):first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<div align="center"><strong>High Quality Image Scraping</strong></div>
+<div align="center">Dozens of urls at a single command distance.</div>
+<br />
+<div align="center">
+<br />
+<div align="center">
+  <sub>Cooked by <a href="https://www.instagram.com/villegas.arts/">Numonu</a> 👨‍🍳</sub>
+</div>
+
+<br />
+
+## 🍕bay scrapper
+
+Generates a json file with links and categories of high quality images
+
+## Installation
+
+#### With NPM
+
+```sh
+npm install pizzabay-scraper
+```
+
+## Getting Started
+
+Just run this command in your project
+
+```sh
+npx pizzabay-scraper <category-of-images> <amount-of-images>
+
+npx pizzabay-scraper kittens 10
+```
---
+++ bin/index.js
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+(async () => {
+    const category = process.argv[2] ?? "colors";
+    const itemsLenght = process.argv[3] ?? 10;
+
+    console.log("[○]Running process...");
+
+    const browser = await puppeteer.launch({headless : "new"});
+    const page = await browser.newPage();
+    await page.goto(`https://pixabay.com/es/images/search/${category}/` , {
+        timeout : 120 * 1000
+    });
+
+    console.log("[○]Scraping...");
+
+    const result = await page.evaluate((itemsLenght) => {
+
+        let container = [...document.querySelectorAll(".cell--B7yKd:nth-child(6n) > .container--MwyXl")];
+        container = container.slice(0,Number(itemsLenght));
+        const result = container.map(e => {
+
+            const image = e.querySelector("img");
+            const tags = image.getAttribute("alt").split(",");
+
+            const imageUrl = e.querySelector('meta[itemprop=contentUrl]').content;
+        
+            return {
+                imageUrl,
+                tags,
+            }
+        })        
+        return JSON.stringify(result);
+    } , itemsLenght)
+
+    console.log("[○]Ready data!");
+
+    await browser.close();
+
+    console.log("[○]Writing json file...");
+
+    fs.writeFile("pizza.json", result, 'utf8', function (err) {
+        if (err) {
+            console.log("[X]An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+        console.log("[○]Success!");
+        process.exit(0);
+    });
+
+})();
